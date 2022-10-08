d7ae20472cce5a119efcec9a7bd6c768567b7e2d
Dayana Mariano <dayanaraquelle@gmail.com>
2022-10-08 20:25:03 -0300

    Add tasklist

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./navbar.css"
+
+function Navbar() {
+  return(
+    <nav className="navbar">
+      <span>React Kanban</span>
+    </nav>
+  )
+}
+
+export default Navbar;
