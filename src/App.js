4ebcafc91cec98419a8d9d94e0aa4ff3f8cd691e
Dayana Mariano <dayanaraquelle@gmail.com>
2022-10-12 18:45:06 -0300

    Add addTask function

--- src/App.js
+++ src/App.js
@@ -2,15 +2,39 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from "./components/Navbar/Navbar";
 import TaskList from './components/TaskList/TaskList';
+import { useState } from 'react';
+
+const task = {
+  id: 0,
+  title: "Nova tarefa",
+  state: "pendente"
+};
+
+let idAcc = 0;
+const generatedId = () => {
+  idAcc = idAcc + 1;
+  return idAcc;
+}
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (title, state) => {
+    const newTask = {
+      id: generatedId(),
+      title,
+      state
+    };
+    setTasks((existingTasks) => {
+      return [...existingTasks, newTask]
+    });
+  };
+
   return (
     <div className="App">
       <Navbar />
       <div className="container">
-        <TaskList title="Pendente" />
-        <TaskList title="Fazendo" />
-        <TaskList title="Completa" />
+        <TaskList title="Pendente " onAddTask={addTask} tasks={tasks} />
       </div>
     </div>
   );
--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -2,15 +2,26 @@ import React from "react";
 import "./tasklist.css"
 import PropTypes from "prop-types"
 
-export default function TaskList({ title }) {
+export default function TaskList({ title, onAddTask, tasks }) {
+  const addTask = () => {
+    onAddTask("Nova tarefa", "pendente");
+  }
+
   return (
     <div className="tasklist">
       <div className="title">{title}</div>
-      <div className="content"></div>
+      <div className="content">
+        {tasks.map((task) => {
+          return <div key={task.id}>{task.title}</div>
+        })}
+      </div>
+      <button onClick={addTask}>Adicionar tarefa</button>
     </div>
   )
 }
 
 TaskList.propTypes = {
-  title: PropTypes.string.isRequired
+  title: PropTypes.string.isRequired,
+  onAddTask: PropTypes.func.isRequired,
+  tasks: PropTypes.array.isRequired
 }
