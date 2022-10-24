160cca3fb070e2df09a0b94665148d27d9813825
Dayana Mariano <dayanaraquelle@gmail.com>
2022-10-24 20:22:10 -0300

    make task editable

---
+++ src/components/TaskItem/TaskItem.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types"
+
+export default function TaskItem({id, title, taskState}) {
+  const [isEditind, setIsEditing] = useState(false);
+  const [editableTitle, setEditableTitle] = useState(title);
+
+  const onTitleChange = (event) => {
+    const newTitle = event.target.value;
+    setEditableTitle(newTitle);
+  };
+
+  const onKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      setIsEditing(false);
+    }
+  }
+
+  if (isEditind) {
+    return <input type="text" value={editableTitle} onChange={onTitleChange} onKeyPress={onKeyPress}/>
+  } else {
+    return <div onClick={(e) => setIsEditing(true)}>{editableTitle}</div>;
+  }
+}
+
+TaskItem.propTypes = {
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  taskState: PropTypes.string.isRequired
+}; 
--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./tasklist.css"
 import PropTypes from "prop-types"
+import TaskItem from "../TaskItem/TaskItem";
 
 export default function TaskList({ title, onAddTask, tasks }) {
   const addTask = () => {
@@ -12,7 +13,14 @@ export default function TaskList({ title, onAddTask, tasks }) {
       <div className="title">{title}</div>
       <div className="content">
         {tasks.map((task) => {
-          return <div key={task.id}>{task.title}</div>
+          return (
+            <TaskItem 
+              key={task.id}
+              id={task.id} 
+              title={task.title} 
+              taskState={task.state}
+            />
+          )
         })}
       </div>
       <button onClick={addTask}>Adicionar tarefa</button>
