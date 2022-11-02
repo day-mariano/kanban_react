9a16bdef7e9555cd69f70be3d0af39cf65b0a297
Dayana Mariano <dayanaraquelle@gmail.com>
2022-11-01 21:05:33 -0300

    make tasks change state

--- src/components/TaskItem/TaskItem.js
+++ src/components/TaskItem/TaskItem.js
@@ -18,6 +18,10 @@ export default function TaskItem({id, title, taskState, onTaskUpdate}) {
     }
   };
 
+  const onTaskStateChange = (event) => {
+    onTaskUpdate(id, title, event.target.value)
+  }
+
   if (isEditind) {
     return (
       <input
@@ -27,7 +31,18 @@ export default function TaskItem({id, title, taskState, onTaskUpdate}) {
       onKeyPress={onKeyPress}/>
     )
   } else {
-    return <div onClick={(e) => setIsEditing(true)}>{editableTitle}</div>;
+    return (
+      <div>
+        <div onClick={(e) => setIsEditing(true)}>
+          {editableTitle}
+        </div>
+        <select onChange={onTaskStateChange} value={taskState}>
+          <option value="Pendente">Pendente</option>
+          <option value="Fazendo">Fazendo</option>
+          <option value="Completa">Completa</option>
+        </select>
+      </div>
+    )
   }
 }
 
